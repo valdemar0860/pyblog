5dbbf38c3864fd38bc78c4e32785413bd629974e
Vova Sharp <egoist4427@gmail.com>
2022-12-07 09:38:38 +0200

    add possibility edit post

--- pyblog/home/views.py
+++ pyblog/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.http import HttpResponse
 
 from .models import Post
@@ -20,6 +20,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'home/post_new.html'
     fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'home/post_edit.html'
+
 
 # def index(request):
 #     # return HttpResponse("Hello World")
